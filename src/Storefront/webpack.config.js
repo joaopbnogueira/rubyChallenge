8a213d5e33a67f75d3b28ed3326afee8c8bc3fab
João Nogueira <joaopbnogueira@gmail.com>
2018-08-01 02:17:17 +0100

    initial react support

---
+++ src/Storefront/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env = {}, argv = {}) => {
+  
+    const isProd = argv.mode === 'production';
+
+    const config = {
+        mode: argv.mode || 'development', // we default to development when no 'mode' arg is passed
+        entry: { 'main': './ClientApp/react-notes.tsx' },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'wwwroot/dist'),
+            publicPath: "/dist/"
+        },
+        resolve: {
+            // Add `.ts` and `.tsx` as a resolvable extension.
+            extensions: [".ts", ".tsx", ".js"]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            })
+        ],
+        module:  {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        isProd ?  MiniCssExtractPlugin.loader : 'style-loader', 
+                        'css-loader' 
+                    ]
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'awesome-typescript-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        }
+    }
+  
+    if (! isProd) {
+        config.devtool = 'eval-source-map';
+    }
+
+    return config;
+};
